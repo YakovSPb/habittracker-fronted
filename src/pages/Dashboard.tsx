41b9afe406fab1b1fd26d66ac5ed19fd0c2c2abb
Yakov <itmeetm@gmail.com>
2025-11-22 20:07:05 +0300

    feat: auth me

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,21 @@
-const Dashboard = () => {
+import { useEffect } from "react";
+import { useAuthStore } from "../store/auth.store";
+
+export default function Dashboard() {
+  const user = useAuthStore((s) => s.user);
+  const loading = useAuthStore((s) => s.loading);
+  const init = useAuthStore((s) => s.init);
+
+  useEffect(() => {
+    init(); // ← загружаем пользователя
+  }, []);
+
+  if (loading) return <div>Загрузка...</div>;
+  if (!user) return <div>Не авторизован</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">Dashboard</header>
+    <div>
+      <h1>Добро пожаловать, {user.email}</h1>
     </div>
   );
-};
-
-export default Dashboard;
+}
